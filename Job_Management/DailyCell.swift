4efa98b08e5d630112ce5767784789dcc10781de
Nguyen Phuong <NguyenPhuong0101>
2023-05-03 15:28:15 +0700

    vs1.2

---
+++ Job_Management/DailyCell.swift
@@ -0,0 +1,18 @@
+//
+//  DailyCell.swift
+//  Job_Management
+//
+//  Created by Bảo Nguyễn on 28/04/2023.
+//
+
+import UIKit
+
+class DailyCell : UITableViewCell
+{
+    
+    @IBOutlet weak var time: UILabel!
+    
+    @IBOutlet weak var event1: UILabel!
+    @IBOutlet weak var event2: UILabel!
+    @IBOutlet weak var event3: UILabel!
+}
